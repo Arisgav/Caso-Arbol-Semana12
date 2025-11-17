f1cd77b1142500e622213ad0bea1a6662ad04996
Arisgav <amgavarrete@uamv.edu.ni>
2025-11-17 00:24:53 -0600

    Correccion de los botones

--- casodearbol/GraFo.Designer.cs
+++ casodearbol/GraFo.Designer.cs
@@ -31,17 +31,24 @@
             this.LblEdificio = new System.Windows.Forms.Label();
             this.LblDistancia = new System.Windows.Forms.Label();
             this.TboxEdificio = new System.Windows.Forms.TextBox();
-            this.LblConexiones = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.TboxDistancia = new System.Windows.Forms.TextBox();
             this.LblOrigen = new System.Windows.Forms.Label();
+            this.TboxOrigen = new System.Windows.Forms.TextBox();
+            this.ListBoxEdificios = new System.Windows.Forms.ListBox();
+            this.BtnAgregarEdificio = new System.Windows.Forms.Button();
+            this.BtnAggConexion = new System.Windows.Forms.Button();
+            this.BtnMostrarConexiones = new System.Windows.Forms.Button();
+            this.BtnRutaMasCorta = new System.Windows.Forms.Button();
+            this.BtnValidarConexidad = new System.Windows.Forms.Button();
+            this.TboxDestino = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // LblEdificio
             // 
             this.LblEdificio.AutoSize = true;
             this.LblEdificio.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblEdificio.Location = new System.Drawing.Point(3, 52);
+            this.LblEdificio.Location = new System.Drawing.Point(3, 19);
             this.LblEdificio.Name = "LblEdificio";
             this.LblEdificio.Size = new System.Drawing.Size(221, 33);
             this.LblEdificio.TabIndex = 0;
@@ -51,7 +58,7 @@
             // 
             this.LblDistancia.AutoSize = true;
             this.LblDistancia.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblDistancia.Location = new System.Drawing.Point(2, 114);
+            this.LblDistancia.Location = new System.Drawing.Point(2, 76);
             this.LblDistancia.Name = "LblDistancia";
             this.LblDistancia.Size = new System.Drawing.Size(149, 37);
             this.LblDistancia.TabIndex = 1;
@@ -60,59 +67,132 @@
             // 
             // TboxEdificio
             // 
-            this.TboxEdificio.Location = new System.Drawing.Point(231, 65);
+            this.TboxEdificio.Location = new System.Drawing.Point(230, 32);
             this.TboxEdificio.Name = "TboxEdificio";
             this.TboxEdificio.Size = new System.Drawing.Size(215, 20);
             this.TboxEdificio.TabIndex = 2;
+            this.TboxEdificio.TextChanged += new System.EventHandler(this.TboxEdificio_TextChanged);
             // 
-            // LblConexiones
+            // TboxDistancia
             // 
-            this.LblConexiones.AutoSize = true;
-            this.LblConexiones.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblConexiones.Location = new System.Drawing.Point(12, 267);
-            this.LblConexiones.Name = "LblConexiones";
-            this.LblConexiones.Size = new System.Drawing.Size(169, 33);
-            this.LblConexiones.TabIndex = 3;
-            this.LblConexiones.Text = "Conexiones";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(187, 280);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(128, 20);
-            this.textBox1.TabIndex = 4;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(157, 131);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(128, 20);
-            this.textBox2.TabIndex = 5;
+            this.TboxDistancia.Location = new System.Drawing.Point(157, 91);
+            this.TboxDistancia.Name = "TboxDistancia";
+            this.TboxDistancia.Size = new System.Drawing.Size(128, 20);
+            this.TboxDistancia.TabIndex = 5;
             // 
             // LblOrigen
             // 
             this.LblOrigen.AutoSize = true;
             this.LblOrigen.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblOrigen.Location = new System.Drawing.Point(12, 183);
+            this.LblOrigen.Location = new System.Drawing.Point(12, 347);
             this.LblOrigen.Name = "LblOrigen";
             this.LblOrigen.Size = new System.Drawing.Size(103, 33);
             this.LblOrigen.TabIndex = 6;
             this.LblOrigen.Text = "Origen";
             // 
+            // TboxOrigen
+            // 
+            this.TboxOrigen.Location = new System.Drawing.Point(124, 360);
+            this.TboxOrigen.Name = "TboxOrigen";
+            this.TboxOrigen.Size = new System.Drawing.Size(100, 20);
+            this.TboxOrigen.TabIndex = 7;
+            this.TboxOrigen.TextChanged += new System.EventHandler(this.TboxOrigen_TextChanged);
+            // 
+            // ListBoxEdificios
+            // 
+            this.ListBoxEdificios.FormattingEnabled = true;
+            this.ListBoxEdificios.Location = new System.Drawing.Point(380, 68);
+            this.ListBoxEdificios.Name = "ListBoxEdificios";
+            this.ListBoxEdificios.Size = new System.Drawing.Size(432, 303);
+            this.ListBoxEdificios.TabIndex = 8;
+            // 
+            // BtnAgregarEdificio
+            // 
+            this.BtnAgregarEdificio.Location = new System.Drawing.Point(471, 8);
+            this.BtnAgregarEdificio.Name = "BtnAgregarEdificio";
+            this.BtnAgregarEdificio.Size = new System.Drawing.Size(129, 54);
+            this.BtnAgregarEdificio.TabIndex = 9;
+            this.BtnAgregarEdificio.Text = "Agregar Edificio";
+            this.BtnAgregarEdificio.UseVisualStyleBackColor = true;
+            this.BtnAgregarEdificio.Click += new System.EventHandler(this.BtnAgregarEdificio_Click);
+            // 
+            // BtnAggConexion
+            // 
+            this.BtnAggConexion.Location = new System.Drawing.Point(40, 176);
+            this.BtnAggConexion.Name = "BtnAggConexion";
+            this.BtnAggConexion.Size = new System.Drawing.Size(75, 66);
+            this.BtnAggConexion.TabIndex = 10;
+            this.BtnAggConexion.Text = "Agregar Conexion";
+            this.BtnAggConexion.UseVisualStyleBackColor = true;
+            this.BtnAggConexion.Click += new System.EventHandler(this.BtnConexion_Click);
+            // 
+            // BtnMostrarConexiones
+            // 
+            this.BtnMostrarConexiones.Location = new System.Drawing.Point(132, 173);
+            this.BtnMostrarConexiones.Name = "BtnMostrarConexiones";
+            this.BtnMostrarConexiones.Size = new System.Drawing.Size(75, 66);
+            this.BtnMostrarConexiones.TabIndex = 11;
+            this.BtnMostrarConexiones.Text = "Mostrar Conexiones";
+            this.BtnMostrarConexiones.UseVisualStyleBackColor = true;
+            this.BtnMostrarConexiones.Click += new System.EventHandler(this.BtnMostrarC_Click);
+            // 
+            // BtnRutaMasCorta
+            // 
+            this.BtnRutaMasCorta.Location = new System.Drawing.Point(236, 333);
+            this.BtnRutaMasCorta.Name = "BtnRutaMasCorta";
+            this.BtnRutaMasCorta.Size = new System.Drawing.Size(138, 54);
+            this.BtnRutaMasCorta.TabIndex = 12;
+            this.BtnRutaMasCorta.Text = "Ruta Mas corta";
+            this.BtnRutaMasCorta.UseVisualStyleBackColor = true;
+            // 
+            // BtnValidarConexidad
+            // 
+            this.BtnValidarConexidad.Location = new System.Drawing.Point(236, 176);
+            this.BtnValidarConexidad.Name = "BtnValidarConexidad";
+            this.BtnValidarConexidad.Size = new System.Drawing.Size(81, 63);
+            this.BtnValidarConexidad.TabIndex = 13;
+            this.BtnValidarConexidad.Text = "Validar Conexidad";
+            this.BtnValidarConexidad.UseVisualStyleBackColor = true;
+            // 
+            // TboxDestino
+            // 
+            this.TboxDestino.Location = new System.Drawing.Point(132, 307);
+            this.TboxDestino.Name = "TboxDestino";
+            this.TboxDestino.Size = new System.Drawing.Size(129, 20);
+            this.TboxDestino.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 294);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 33);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Destino";
+            // 
             // GraFo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(716, 450);
+            this.ClientSize = new System.Drawing.Size(1011, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TboxDestino);
+            this.Controls.Add(this.BtnValidarConexidad);
+            this.Controls.Add(this.BtnRutaMasCorta);
+            this.Controls.Add(this.BtnMostrarConexiones);
+            this.Controls.Add(this.BtnAggConexion);
+            this.Controls.Add(this.BtnAgregarEdificio);
+            this.Controls.Add(this.ListBoxEdificios);
+            this.Controls.Add(this.TboxOrigen);
             this.Controls.Add(this.LblOrigen);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.LblConexiones);
+            this.Controls.Add(this.TboxDistancia);
             this.Controls.Add(this.TboxEdificio);
             this.Controls.Add(this.LblDistancia);
             this.Controls.Add(this.LblEdificio);
             this.Name = "GraFo";
             this.Text = "GraFo";
+            this.Load += new System.EventHandler(this.GraFo_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,9 +203,16 @@
         private System.Windows.Forms.Label LblEdificio;
         private System.Windows.Forms.Label LblDistancia;
         private System.Windows.Forms.TextBox TboxEdificio;
-        private System.Windows.Forms.Label LblConexiones;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox TboxDistancia;
         private System.Windows.Forms.Label LblOrigen;
+        private System.Windows.Forms.TextBox TboxOrigen;
+        private System.Windows.Forms.ListBox ListBoxEdificios;
+        private System.Windows.Forms.Button BtnAgregarEdificio;
+        private System.Windows.Forms.Button BtnAggConexion;
+        private System.Windows.Forms.Button BtnMostrarConexiones;
+        private System.Windows.Forms.Button BtnRutaMasCorta;
+        private System.Windows.Forms.Button BtnValidarConexidad;
+        private System.Windows.Forms.TextBox TboxDestino;
+        private System.Windows.Forms.Label label1;
     }
 }
--- casodearbol/GraFo.cs
+++ casodearbol/GraFo.cs
@@ -1,25 +1,257 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace casodearbol
 {
     public partial class GraFo : Form
     {
+        Dictionary<string, List<(string destino, int distancia)>> grafo =
+            new Dictionary<string, List<(string destino, int distancia)>>();
+
         public GraFo()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void BtnAgregarEdificio_Click(object sender, EventArgs e)
         {
+            string edificio = TboxEdificio.Text.Trim();
 
+            if (edificio == "")
+            {
+                MessageBox.Show("Ingrese el nombre del edificio.");
+                return;
+            }
+
+            if (!grafo.ContainsKey(edificio))
+            {
+                grafo.Add(edificio, new List<(string, int)>());
+                ListBoxEdificios.Items.Add(edificio);
+                MessageBox.Show("Edificio agregado correctamente.");
+            }
+            else
+            {
+                MessageBox.Show("Ese edificio ya existe.");
+            }
+
+            TboxEdificio.Clear();
         }
+
+        private void BtnConexion_Click(object sender, EventArgs e)
+        {
+            string origen = TboxOrigen.Text.Trim();
+            string destino = TboxDestino.Text.Trim();
+
+            if (!int.TryParse(TboxDistancia.Text, out int distancia) || distancia <= 0)
+            {
+                MessageBox.Show("Ingrese una distancia válida (número mayor a 0).");
+                return;
+            }
+
+            if (origen == "" || destino == "")
+            {
+                MessageBox.Show("Ingrese origen y destino.");
+                return;
+            }
+
+            if (!grafo.ContainsKey(origen) || !grafo.ContainsKey(destino))
+            {
+                MessageBox.Show("Ambos edificios deben existir en el grafo.");
+                return;
+            }
+
+            if (origen == destino)
+            {
+                MessageBox.Show("No se puede conectar un edificio consigo mismo.");
+                return;
+            }
+
+            // Evitar duplicados
+            if (grafo[origen].Any(x => x.destino == destino))
+            {
+                MessageBox.Show("Ya existe una conexión entre esos edificios.");
+                return;
+            }
+
+            // Agregar conexión bidireccional
+            grafo[origen].Add((destino, distancia));
+            grafo[destino].Add((origen, distancia));
+
+            MessageBox.Show($"Conexión agregada: {origen} <-> {destino} ({distancia})");
+
+            TboxOrigen.Clear();
+            TboxDestino.Clear();
+            TboxDistancia.Clear();
+        }
+
+        private void BtnMostrarC_Click(object sender, EventArgs e)
+        {
+            if (grafo.Count == 0)
+            {
+                MessageBox.Show("No hay edificios en el grafo.");
+                return;
+            }
+
+            string conexiones = "CONEXIONES DEL GRAFO:\n\n";
+
+            foreach (var edificio in grafo)
+            {
+                conexiones += $"{edificio.Key}:\n";
+
+                if (edificio.Value.Count == 0)
+                {
+                    conexiones += "  Sin conexiones\n";
+                }
+                else
+                {
+                    foreach (var conexion in edificio.Value)
+                    {
+                        conexiones += $"  -> {conexion.destino} ({conexion.distancia})\n";
+                    }
+                }
+                conexiones += "\n";
+            }
+
+            MessageBox.Show(conexiones, "Conexiones del Parque");
+        }
+
+        private void BtnValidarConexidad_Click(object sender, EventArgs e)
+        {
+            if (grafo.Count == 0)
+            {
+                MessageBox.Show("El grafo está vacío.");
+                return;
+            }
+
+            if (EsGrafoConexo())
+            {
+                MessageBox.Show("✅ El grafo ES CONEXO - Todos los edificios están conectados.");
+            }
+            else
+            {
+                MessageBox.Show("❌ El grafo NO ES CONEXO - Hay edificios aislados.");
+            }
+        }
+
+        private bool EsGrafoConexo()
+        {
+            var visitados = new HashSet<string>();
+            var cola = new Queue<string>();
+
+            var inicio = grafo.Keys.First();
+            visitados.Add(inicio);
+            cola.Enqueue(inicio);
+
+            while (cola.Count > 0)
+            {
+                string actual = cola.Dequeue();
+
+                foreach (var vecino in grafo[actual])
+                {
+                    if (visitados.Add(vecino.destino))
+                        cola.Enqueue(vecino.destino);
+                }
+            }
+
+            return visitados.Count == grafo.Count;
+        }
+
+        private void BtnRutaMasCorta_Click(object sender, EventArgs e)
+        {
+            string origen = TboxOrigen.Text.Trim();
+            string destino = TboxDestino.Text.Trim();
+
+            if (origen == "" || destino == "")
+            {
+                MessageBox.Show("Ingrese origen y destino para calcular la ruta.");
+                return;
+            }
+
+            if (!grafo.ContainsKey(origen) || !grafo.ContainsKey(destino))
+            {
+                MessageBox.Show("Ambos edificios deben existir en el grafo.");
+                return;
+            }
+
+            var (distancia, ruta) = Dijkstra(origen, destino);
+
+            if (distancia == int.MaxValue)
+            {
+                MessageBox.Show($"No hay ruta posible entre {origen} y {destino}.");
+                return;
+            }
+
+            MessageBox.Show(
+                $"Ruta más corta: {string.Join(" -> ", ruta)}\nDistancia total: {distancia}",
+                "Ruta Calculada"
+            );
+        }
+
+        private (int distancia, List<string> ruta) Dijkstra(string origen, string destino)
+        {
+            var dist = new Dictionary<string, int>();
+            var prev = new Dictionary<string, string>();
+            var pendientes = new HashSet<string>(grafo.Keys);
+
+            foreach (var nodo in grafo.Keys)
+            {
+                dist[nodo] = int.MaxValue;
+                prev[nodo] = null;
+            }
+
+            dist[origen] = 0;
+
+            while (pendientes.Count > 0)
+            {
+                string actual = pendientes
+                    .OrderBy(n => dist[n])
+                    .FirstOrDefault();
+
+                if (actual == null || dist[actual] == int.MaxValue)
+                    break;
+
+                pendientes.Remove(actual);
+
+                if (actual == destino)
+                    break;
+
+                foreach (var vecino in grafo[actual])
+                {
+                    if (!pendientes.Contains(vecino.destino))
+                        continue;
+
+                    int nuevaDist = dist[actual] + vecino.distancia;
+
+                    if (nuevaDist < dist[vecino.destino])
+                    {
+                        dist[vecino.destino] = nuevaDist;
+                        prev[vecino.destino] = actual;
+                    }
+                }
+            }
+
+            if (dist[destino] == int.MaxValue)
+                return (int.MaxValue, new List<string>());
+
+            // Reconstruir ruta
+            List<string> ruta = new List<string>();
+            string actualRuta = destino;
+
+            while (actualRuta != null)
+            {
+                ruta.Insert(0, actualRuta);
+                actualRuta = prev[actualRuta];
+            }
+
+            return (dist[destino], ruta);
+        }
+
+        // Eventos vacíos válidos que sí pueden existir
+        private void label1_Click(object sender, EventArgs e) { }
+        private void TboxOrigen_TextChanged(object sender, EventArgs e) { }
+        private void TboxEdificio_TextChanged(object sender, EventArgs e) { }
+        private void GraFo_Load(object sender, EventArgs e) { }
     }
 }
